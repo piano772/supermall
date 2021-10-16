b308b2b9bca52a597d8a61dde37c8691b11b48e3
saaaaatina <83028022@qq.com>
2021-10-16 16:30:17 +0800

    配置提交

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
- 
+  <div id="app">
+  
+  <main-tab-bar/>
+
+  </div>
+  
 </template>
 
 <script>
+import MainTabBar from "../components/content/mainTabbar"
+import MainTabBar from '../components/content/mainTabbar/MainTabBar.vue'
 
 export default {
+  components: { MainTabBar },
   name: 'App',
+  MainTabBar
 
 }
 </script>
 
 <style>
-
+ @import "../assets/css/base.css";
 </style>
